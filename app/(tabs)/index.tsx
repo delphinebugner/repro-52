92ef5e70c3bfdd3301e965f19b04a4791952f249
Delphine Bugner <delphine@mistral.ai>
2025-03-13 16:24:44 +0100

    feat :sparkles: (TrueSheet) add repro Android

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
-import { Image, StyleSheet, Platform, Button } from "react-native";
+import { Button, Image, StyleSheet, View } from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { TrueSheet } from "@lodev09/react-native-true-sheet";
 
 export default function HomeScreen() {
+  const sheet = useRef<TrueSheet>(null);
+  // Present the sheet ✅
+  const present = async () => {
+    await sheet.current?.present();
+    console.log("horray! sheet has been presented 💩");
+  };
+
+  // Dismiss the sheet ✅
+  const dismiss = async () => {
+    await sheet.current?.dismiss();
+    console.log("Bye bye 👋");
+  };
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -22,12 +36,43 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Repro Issue Modal iOS:</ThemedText>
-        <Button onPress={() => router.navigate("/modal")} title="Open Modal" />
+        <ThemedText type="subtitle">
+          Repro Issue Modal iOS (react-native-screens/issues/2770):
+        </ThemedText>
+        <Button
+          onPress={() => router.navigate("/modal")}
+          title="Open Modal / formSheet"
+        />
+        <Button
+          onPress={() => router.navigate("/full-screen-modal")}
+          title="Open FullScreen Modal"
+        />
+        <ThemedText type="subtitle">
+          Repro Issue Pressable True Sheet Android
+          (lodev09/react-native-true-sheet/issues/163):
+        </ThemedText>
+        <Button onPress={present} title="Open True Sheet" />
+        <TrueSheet ref={sheet} sizes={["auto", "large"]} cornerRadius={24}>
+          <TrueSheetContent />
+        </TrueSheet>
       </ThemedView>
     </ParallaxScrollView>
   );
 }
+
+const TrueSheetContent = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <ThemedView className="px-5 py-12">
+      <ThemedText>True Sheet Content</ThemedText>
+      <View className="h-1" />
+      <Button
+        title={`Count: ${count}`}
+        onPress={() => setCount((c) => c + 1)}
+      />
+    </ThemedView>
+  );
+};
 
 const styles = StyleSheet.create({
   titleContainer: {
